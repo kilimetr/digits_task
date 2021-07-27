de22e77b1bcba2f340d47ec3a04c2547b8158b98
Dominik Capkovic <domcapkovic@gmail.com>
2021-07-27 22:36:22 +0200

    regex path changed

--- WEATHER/weather_forecast/urls.py
+++ WEATHER/weather_forecast/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from weather_forecast import views
 
@@ -6,7 +6,8 @@ from weather_forecast import views
 
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("?date={<date>}&country_code={<country_code>}/", views.jresp_result, name = "jresp_result")
+    re_path(r"^(?P<date>[\w-]+)/(?P<country_code>[\w-]+)/$", views.jresp_result, name = "jresp_result")
+    # re_path(r"^[\w-]+/$", views.jresp_result, name = "jresp_result")
 ]
 
 
--- WEATHER/weather_forecast/views.py
+++ WEATHER/weather_forecast/views.py
@@ -2,16 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
 import json
+import datetime
 
 
 
 # Create your views here.
 def index(request):
     url = "http://api.weatherapi.com/v1/forecast.json?key=361f4a39a7cb4ab892d131558211807&q={}&days={}"
-
-    # vzít z modelu
-    # country = Weather_Update.objects.all()[0]
-    # date    = Weather_Update.date
 
     country = "UK"
     date = "2021-07-06"
@@ -74,13 +71,80 @@ def index(request):
 
 
 def jresp_result(request, date, country_code):
-    json_dict = {
-        "date": date,
-        "country_code": country_code
-    }
+    date_separ = date.split("-")
 
-    print(json_dict)
+    today_date       = datetime.date.today()
+    today_date_year  = today_date.year # type int
+    today_date_month = today_date.month
+    today_date_day   = today_date.day
 
-    return JsonResponse(json_dict)
+
+    date_separ = date.split("-") # year, day, month
+
+    i = 0
+    for item in date_separ:
+        date_separ[i] = int(date_separ[i])
+        i = i +1
+
+    date_year  = date_separ[0] # type int
+    date_month = date_separ[2]
+    date_day   = date_separ[1]
+
+
+    if date_year != today_date_year:
+        print("!!! SEEMS THAT YEAR INPUT IS NOT CORRECT !!!")
+        return
+
+    if date_month != today_date_month:
+        print("!!! SEEMS THAT MONTH INPUT IS NOT CORRECT !!!")
+        return
+
+    day_dt = date_day - today_date_day
+
+
+    if country_code == "CZ":
+        city = "Prague"
+
+    elif country_code == "SK":
+        city = "Bratislava"
+
+    elif country_code == "UK":
+        city = "London"
+
+
+    days = day_dt + 1
+
+    url = "http://api.weatherapi.com/v1/forecast.json?key=361f4a39a7cb4ab892d131558211807&q={}&days={}"
+
+    response = requests.get(url.format(city, days))
+
+    if response.status_code != 200:
+        print("\n \n \n!!! SORRY, CRASHED !!!\n \n \n")
+
+    response = requests.get(url.format(city, days)).json()
+
+    weather = {
+        "country": country_code,
+        "city": response["location"]["name"],
+        "date": response["forecast"]["forecastday"][0]["date"],
+        "temp_av": response["forecast"]["forecastday"][0]["day"]["avgtemp_c"]
+        }
+
+    context = {"weather": weather}
+
+
+    if context["weather"]["temp_av"] > 20:
+        answer = "good"
+
+    elif 10 <= context["weather"]["temp_av"] <= 20:
+        answer = "soso"
+
+    else:
+        answer = "bad"
+
+    final = {"forecast": answer}
+
+
+    return JsonResponse(final)
 
 
